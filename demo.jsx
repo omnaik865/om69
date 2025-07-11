832210bcddd3f74931a92d5ef88255722c1369da
omnaik865 <naikom865@gmail.com>
2025-07-11 09:10:56 +0530

    Add files via upload

---
+++ demo.jsx
@@ -0,0 +1,12 @@
+function Demo(props){
+return(
+    <div>
+         <h2> This is demo file....</h2>
+    <h3>employee name{props.emp}</h3>
+    <h4>employee email{props.emp}</h4>
+    </div>
+       
+
+)
+}
+export default Demo;
